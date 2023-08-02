fa7070ef4ac6df805cc5a6a0e6c4098e81e5b3bf
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-08-02 19:03:20 -0300

    Configuring Roles Authorization inside web app using Spring Security

--- springsecsection7/src/main/java/com/gustavo/config/ProjectSecurityConfig.java
+++ springsecsection7/src/main/java/com/gustavo/config/ProjectSecurityConfig.java
@@ -69,10 +69,14 @@ public class ProjectSecurityConfig {
 			// Filtro responsável por enviar o cookie e o valor do cabeçalho para o front-end após o login inicial.
 			.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
 			.authorizeHttpRequests((requests) -> requests		
-				.requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
+				/*.requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
 				.requestMatchers("/myBalance").hasAnyAuthority("VIEWACCOUNT", "VIEWBALANCE")
 				.requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
-				.requestMatchers("/myCards").hasAuthority("VIEWCARDS")
+				.requestMatchers("/myCards").hasAuthority("VIEWCARDS")*/
+				.requestMatchers("/myAccount").hasRole("USER")
+				.requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+				.requestMatchers("/myLoans").hasRole("USER")
+				.requestMatchers("/myCards").hasRole("USER")
 				.requestMatchers("/user").authenticated()
 				.requestMatchers("/notices","/contact","/register").permitAll())
 			.formLogin(Customizer.withDefaults()) // Permite que os usuários se autentiquem com login baseado em formulário
--- springsecsection7/src/main/resources/sql/scripts.sql
+++ springsecsection7/src/main/resources/sql/scripts.sql
@@ -181,8 +181,16 @@ INSERT INTO `authorities` (`customer_id`, `name`)
 INSERT INTO `authorities` (`customer_id`, `name`)
  VALUES (1, 'VIEWCARDS');
 
- INSERT INTO `authorities` (`customer_id`, `name`)
-  VALUES (1, 'VIEWLOANS');
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'VIEWLOANS');
 
- INSERT INTO `authorities` (`customer_id`, `name`)
-   VALUES (1, 'VIEWBALANCE');
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'VIEWBALANCE');
+   
+DELETE FROM `authorities`;
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'ROLE_USER');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'ROLE_ADMIN');
